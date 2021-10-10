cc5642cbd2b1df1c3d5654d136dd012e2ec941fb
goncrust <goncalonrua@gmail.com>
2021-10-10 23:25:11 +0100

    add leic-a

---
+++ LEIC-A/FP/cap2/ex1.py
@@ -0,0 +1,9 @@
+
+print("Vou pedir-lhe dois numeros")
+
+x = eval(input("Escreva o primeiro numero, x = "))
+y = eval(input("Escreva o segundo numero, y = "))
+
+resultado = (x + 3 * y) * (x - y)
+
+print("O valor de (x + 3 * y) * (x - y) e:", resultado)
