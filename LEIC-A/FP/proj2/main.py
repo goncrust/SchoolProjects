bd207fae81ffe2e02388ab6e5ae4ee98e852b381
goncrust <goncalonrua@gmail.com>
2021-11-15 19:53:01 +0000

    proj2, TAD animal

--- LEIC-A/FP/proj2/main.py
+++ LEIC-A/FP/proj2/main.py
@@ -1,4 +1,5 @@
-
+"""
+"""
 
 # TAD posicao - Operação Básicas
 
@@ -68,7 +69,7 @@ def posicoes_iguais(p1, p2):
     (posicao x posicao --> booleano)
     """
 
-    return eh_posicao(p1) and eh_posicao(p2) and p1[0] == p2[0] and p1[1] == p2[1]
+    return eh_posicao(p1) and eh_posicao(p2) and p1 == p2
 
 
 def posicao_para_str(p):
@@ -81,6 +82,7 @@ def posicao_para_str(p):
     return f'({p[0]}, {p[1]})'
 
 # TAD posicao - Funções de alto nível
+
 
 def obter_posicoes_adjacentes(p):
     """Determina as posicoes adjacentes
@@ -145,4 +147,254 @@ def ordenar_posicoes(t):
             break
 
     return tuple(l)
+
+# TAD animal - Operações Básicas
+
+
+def cria_animal(s, r, a):
+    """Construtor do TAD animal
+
+    Recebe uma string (espécie do animal) e dois inteiros (frequência de reprodução e
+    frequência de alimentação, respetivamente), e devolve uma nova variável do tipo animal,
+    com essas propriedades. Se a frequência de alimentação for 0, o animal é considerado 
+    uma presa, caso contrário é considerado um predador. Verifica a validade dos argumentos.
+    (str x int x int --> animal)
+    """
+
+    if not isinstance(s, str) or not isinstance(r, int) or not isinstance(a, int) \
+            or r < 0 or a < 0:
+
+        raise ValueError('cria_animal: argumentos invalidos')
+
+    return {'especie': s, 'freq_reproducao': r, 'freq_alimentacao': a, 'idade': 0, 'fome': 0}
+
+
+def cria_copia_animal(a):
+    """Construtor do TAD animal que cria uma cópia de um animal existente
+
+    Recebe uma variável animal e devolve uma nova variável animal igual ao recebido (com as
+    mesmas propriedades).
+    (animal --> animal)
+    """
+
+    return {'especie': a['especie'], 'freq_reproducao': a['freq_reproducao'],
+            'freq_alimentacao': a['freq_alimentacao'], 'idade': 0, 'fome': 0}
+
+
+def obter_especie(a):
+    """Devolve a espécie do animal
+
+    Recebe um animal e devolve a sua espécie.
+    (animal --> str)
+    """
+
+    return a['especie']
+
+
+def obter_freq_reproducao(a):
+    """Devolve a frequência de reprodução do animal
+
+    Recebe um animal e devolve a sua frequência de reprodução.
+    (animal --> int)
+    """
+
+    return a['freq_reproducao']
+
+
+def obter_freq_alimentacao(a):
+    """Devolve a frequência de alimentação do animal
+
+    Recebe um animal e devolve a sua frequência de alimentação.
+    (animal --> int)
+    """
+
+    return a['freq_alimentacao']
+
+
+def obter_idade(a):
+    """Devolve a idade do animal
+
+    Recebe um animal e devolve a sua idade.
+    (animal --> int)
+    """
+
+    return a['idade']
+
+
+def obter_fome(a):
+    """Devolve a fome do animal
+
+    Recebe um animal e devolve o valor da sua fome.
+    (animal --> int)
+    """
+
+    return a['fome']
+
+
+def aumenta_idade(a):
+    """Incrementa a idade do animal
+
+    Recebe um animal e modifica-o destrutivamente, incrementando a sua idade.
+    Devolve o próprio animal.
+    (animal --> animal)
+    """
+
+    a['idade'] += 1
+    return a
+
+
+def reset_idade(a):
+    """Repõe a idade do animal para zero
+
+    Recebe um animal e modifica-o destrutivamente, repondo o valor da sua idade para zero.
+    Devolve o próprio animal.
+    (animal --> animal)
+    """
+
+    a['idade'] = 0
+    return a
+
+
+def aumenta_fome(a):
+    """Incrementa a fome do animal
+
+    Recebe um animal e modifica-o destrutivamente, incrementando a sua fome.
+    Devolve o próprio animal.
+    (animal --> animal)
+    """
+
+    a['fome'] += 1
+    return a
+
+
+def reset_fome(a):
+    """Repõe a fome do animal para zero
+
+    Recebe um animal e modifica-o destrutivamente, repondo o valor da sua fome para zero.
+    Devolve o próprio animal.
+    (animal --> animal)
+    """
+
+    a['fome'] = 0
+    return a
+
+
+def eh_animal(arg):
+    """Determina se uma variável corresponde ao tipo animal
+
+    Recebe um argumento de qualquer tipo e devolve True se o mesmo corresponde
+    a um TAD animal. Caso contrário devolve False.
+    (universal --> booleano)
+    """
+
+    return isinstance(arg, dict) and 'especie' in arg and 'freq_reproducao' in arg and \
+            'freq_alimentacao' in arg and 'idade' in arg and 'fome' in arg and \
+            isinstance(arg['especie'], str) and isinstance(arg['freq_reproducao'], int) and \
+            isinstance(arg['freq_alimentacao'], int) and isinstance(arg['idade'], int) and \
+            isinstance(arg['fome'], int) and arg['freq_reproducao'] >= 0 and \
+            arg['freq_alimentacao'] >= 0 and arg['idade'] >= 0 and arg['fome'] >= 0
+
+
+def eh_predador(arg):
+    """Determina se uma variável corresponde a um animal predador
+
+    Recebe um argumento de qualquer tipo e devolve True se o mesmo corresponde
+    a um TAD animal, predador. Um animal é predador se a sua frequência de alimentação
+    for superior a zero. Caso não seja devolve False.
+    (universal --> booleano)
+    """
+
+    return eh_animal(arg) and arg['freq_alimentacao'] > 0
+
+
+def eh_presa(arg):
+    """Determina se uma variável corresponde a um animal presa
+
+    Recebe um argumento de qualquer tipo e devolve True se o mesmo corresponde
+    a um TAD animal, presa. Um animal é presa se a sua frequência de alimentação
+    for igual a zero. Caso não seja devolve False.
+    (universal --> booleano)
+    """
+
+    return eh_animal(arg) and arg['freq_alimentacao'] == 0
+
+
+def animais_iguais(a1, a2):
+    """Verifica se os animais são iguais
+
+    Recebe dois TAD animal e devolve True apenas se ambos representam o mesmo animal,
+    isto é, a mesma espécie e propriedades.
+    Caso contrário devolve False.
+    (animal x animal --> booleano)
+    """
+
+    return eh_animal(a1) and eh_animal(a2) and a1 == a2
+
+
+def animal_para_char(a):
+    """Representação externa da espécie
+
+    Recebe um animal e devolve uma string de um único elemento que corresponde ao primeiro 
+    carácter da espécie do animal, em maiúscula se for predador e em minúscula se for presa.
+    (animal --> str)
+    """
+
+    if eh_predador(a):
+        return a['especie'][0].upper()
+    elif eh_presa(a):
+        return a['especie'][0].lower()
+
+
+def animal_para_str(a):
+    """Representação externa do tipo animal
+
+    Devolve uma string que corresponde à representação externa do TAD animal.
+    (animal --> str)
+    """
+
+    res = f"{a['especie']} [{a['idade']}/{a['freq_reproducao']}"
+    if eh_predador(a):
+        res += f";{a['fome']}/{a['freq_alimentacao']}"
+
+    return res + ']'
+
+# TAD animal - Funções de alto nível
+
+
+def eh_animal_fertil(a):
+    """Verifica se o animal atingiu a idade de reprodução
+
+    Recebe um animal e devolve True se o mesmo atingiu a idade de reprodução.
+    Caso contrário devolve False.
+    (animal --> booleano)
+    """
+
+    return obter_idade(a) >= obter_freq_reproducao(a)
+
+
+def eh_animal_faminto(a):
+    """Verifica se o animal atingiu o estado de faminto
+
+    Recebe um animal e devolve True se o mesmo atingiu um valor de fome igual ou superior 
+    à sua frequência de alimentação. Caso contrário devolve False. 
+    As presas devolvem sempre False.
+    (animal --> booleano)
+    """
+
+    if eh_presa(a):
+        return False
+
+    return obter_fome(a) >= obter_freq_alimentacao(a)
+
+
+def reproduz_animal(a):
+    """Efetua a reprodução de um animal
+
+    Recebe um animal e devolve um novo animal da mesma espécie com idade e fome igual a zero.
+    O animal que deu origem a este (argumento) é modificado destrutivamente, alterando a sua
+    idade para zero.
+    """
+
+    reset_idade(a)
+    return cria_copia_animal(a)
 
