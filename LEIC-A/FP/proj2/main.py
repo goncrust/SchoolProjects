a533d7961317ac5fcf28b880478a0d04b5b866b3
goncrust <goncalonrua@gmail.com>
2021-11-16 21:55:47 +0000

    proj2, TAD prado

--- LEIC-A/FP/proj2/main.py
+++ LEIC-A/FP/proj2/main.py
@@ -56,9 +56,7 @@ def eh_posicao(arg):
     a um TAD posicao. Caso contrário devolve False.
     (universal --> booleano)
     """
-            # Propriedades do tuplo
-            # Tipo dos elementos
-            # Propriedades dos elementos
+
     return isinstance(arg, tuple) and len(arg) == 2 and \
             isinstance(arg[0], int) and isinstance(arg[1], int) \
             and arg[0] >= 0 and arg[1] >= 0
@@ -260,14 +258,6 @@ def eh_animal(arg):
     a um TAD animal. Caso contrário devolve False.
     (universal --> booleano)
     """
-            # Propriedades do dicionário
-            # Chaves do dicionário
-
-            # Tipo dos valores
-
-
-            # Propriedades dos valores
-
     return isinstance(arg, dict) and len(arg) == 5 and \
             'especie' in arg and 'freq_reproducao' in arg and 'freq_alimentacao' in arg and \
             'idade' in arg and 'fome' in arg and \
@@ -376,12 +366,61 @@ def reproduz_animal(a):
     Recebe um animal e devolve um novo animal da mesma espécie com idade e fome igual a zero.
     O animal que deu origem a este (argumento) é modificado destrutivamente, alterando a sua
     idade para zero.
+    (animal --> animal)
     """
 
     reset_idade(a)
     return cria_copia_animal(a)
 
 # TAD prado - Operações Básicas
+
+
+# Função auxiliar
+def obter_indice_pos_tuplo(t, p):
+    """Indice de um TAD posicao no tuplo
+
+    Recebe um tuplo e uma variável do tipo posicao e devolve o indice em que
+    essa posicao aparece no tuplo pela primeira vez. Caso a mesma não esteja
+    presente no tuplo, é devolvido -1.
+    (tuplo x posicao --> int)
+    """
+
+    for i in range(len(t)):
+        if posicoes_iguais(t[i], p):
+            return i
+
+    return -1
+
+
+# Função auxiliar
+def eh_limite_exterior_prado(ult_pos, p):
+    """Verifica se posicao pertence aos limites exteriores do prado
+
+    Recebe a posicao que corresponde à montanha do canto inferior direito do prado
+    e uma posicao. Devolve True caso a posicao faça parte dos limites exteriores do
+    prado. Caso contrário devolve False.
+    (posicao x posicao --> booleano)
+    """
+
+    x = obter_pos_x(p)
+    y = obter_pos_y(p)
+    return x == 0 or y == 0 or x == obter_pos_x(ult_pos) or y == obter_pos_y(ult_pos)
+
+
+# Função auxiliar
+def existe_pos_repetida(t):
+    """Verifica posições repetidas num tuplo
+
+    Recebe um tuplo com elementos que são do tipo posicao e devolve True caso alguma
+    esteja repetida no tuplo. Caso contrário devolve False.
+    (tuplo --> booleano)
+    """
+
+    for i in range(len(t)):
+        if obter_indice_pos_tuplo(t[:i], t[i]) != -1:
+            return True
+
+    return False
 
 
 def cria_prado(d, r, a, p):
@@ -391,19 +430,25 @@ def cria_prado(d, r, a, p):
     ou mais posições (de rochedos que não são montanhas dos limites exteriores do prado),
     um tuplo com um ou mais animais e um tuplo do mesmo tamanho do anterior com as posições
     correspondentes aos animais. Devolve uma nova variável do tipo prado com os atributos
-    recebidos. Verifica a validade dos argumentos.
+    recebidos.
+    Verifica a validade dos argumentos:
+        1. Tipos dos argumentos
+        2. Tamanho dos tuplos
+        3. Tipos dos elementos dos tuplos
+        4. Se existem rochedos nos limites exteriores
+        5. Se existem animais nos limites exteriores
+        6. Se existem animais em rochedos
+        7. Se existem posições de animais/rochedos repetidas
     (posicao x tuplo x tuplo x tuplo --> prado)
     """
 
-    # Será preciso verificar que os rochedos no r não pertencem aos limites exteriores do prado?
-    # E que os animais não estão em cima de rochedos?
-    # E que os animais não estão em cima de outros animais?
-    # Será preciso fazer isto no eh_prado?
-
     if not eh_posicao(d) or not isinstance(r, tuple) or not isinstance(a, tuple) or \
             not isinstance(p, tuple) or len(a) < 1 or len(a) != len(p) or \
-            not all(map(lambda x: eh_posicao(x), r)) or \
-            not all(map(lambda x, y: eh_animal(x) and eh_posicao(y), a, p)):
+            not all(map(lambda x: eh_posicao(x) and not eh_limite_exterior_prado(d, x), r)) or \
+            not all(map(lambda x, y: eh_animal(x) and eh_posicao(y) 
+                and not eh_limite_exterior_prado(d, y) 
+                and obter_indice_pos_tuplo(r, y) == -1, a, p)) or \
+            existe_pos_repetida(r) or existe_pos_repetida(p):
 
         raise ValueError('cria_prado: argumentos invalidos')
 
@@ -411,39 +456,73 @@ def cria_prado(d, r, a, p):
 
 
 def cria_copia_prado(m):
+    """Construtor do TAD prado que cria uma cópia de um prado existente
+
+    Recebe um prado e devolve um novo prado igual ao recebido.
+    (prado --> prado)
+    """
+
     return m.copy()
 
 
 def obter_tamanho_x(m):
+    """Devolve o tamanho do prado no eixo x
+
+    Recebe um prado e devolve o tamanho do seu eixo x.
+    (prado --> int)
+    """
+
     return obter_pos_x(m['ult_pos']) + 1
 
 
 def obter_tamanho_y(m):
+    """Devolve o tamanho do prado no eixo y
+
+    Recebe um prado e devolve o tamanho do seu eixo y.
+    (prado --> int)
+    """
+
     return obter_pos_y(m['ult_pos']) + 1
 
 
 def obter_numero_predadores(m):
+    """Devolve o número de predadores no prado
+
+    Recebe um prado e devolve o número de predadores atualmento no mesmo.
+    (prado --> int)
+    """
+
     return len([x for x in m['animais'] if eh_predador(x)])
 
 
 def obter_numero_presas(m):
+    """Devolve o número de presas no prado
+
+    Recebe um prado e devolve o número de presas atualmento no mesmo.
+    (prado --> int)
+    """
+
     return len([x for x in m['animais'] if eh_presa(x)])
 
 
 def obter_posicao_animais(m):
+    """Devolve o tuplo corresponde às posições dos animais
+
+    Recebe um prado e devolve o tuplo que contem as posições dos animais do prado.
+    (prado --> tuplo posicoes)
+    """
+
     return ordenar_posicoes(m['pos_animais'])
 
 
-# Função auxiliar
-def obter_indice_pos_tuplo(t, p):
-    for i in range(len(t)):
-        if posicoes_iguais(t[i], p):
-            return i
-
-    return -1
-
-
 def obter_animal(m, p):
+    """Devolve o animal na posicao
+
+    Recebe um prado e uma posicao e devolve o animal que está nessa posicao, no prado.
+    Caso não exista um animal nessa posicao, devolve None.
+    (prado x posicao --> animal)
+    """
+
     i = obter_indice_pos_tuplo(m['pos_animais'], p)
     if i == -1:
         return None
@@ -451,75 +530,133 @@ def obter_animal(m, p):
 
 
 def eliminar_animal(m, p):
+    """Elimina o animal na posicao
 
-    i = obter_indice_pos_tuplo(m['pos_animais'], p)
-    if i == -1:
-        return m
+    Recebe um prado e uma posicao e altera destrutivamente o prado, eliminando o animal
+    que estava nessa posicao. Caso o animal não exista, nada é alterado.
+    Devolve o próprio prado.
+    (prado x posicao --> prado)
+    """
 
-    # eliminar entradas do animal e posicao do animal
-    m['pos_animais'] = m['pos_animais'][:i] + m['pos_animais'][i+1:]
-    m['animais'] = m['animais'][:i] + m['animais'][i+1:]
+    if eh_posicao_animal(m, p):
+        # eliminar entradas do animal e posicao do animal
+        m['pos_animais'] = m['pos_animais'][:i] + m['pos_animais'][i+1:]
+        m['animais'] = m['animais'][:i] + m['animais'][i+1:]
 
     return m
 
 
 def mover_animal(m, p1, p2):
+    """Move o animal de uma posicao para outra
 
-    i = obter_indice_pos_tuplo(m['pos_animais'], p)
-    if i == -1:
-        return m
-    m['pos_animais'] = m['pos_animais'][:i] + (p2,) + m['pos_animais'][i+1:]
+    Recebe um prado e duas variáveis do tipo posicao e altera destrutivamente o prado,
+    movendo o animal que estava na primeira posicao, para a segunda.
+    Caso o animal não exista ou a posicao de destino já esteja ocupada, nada é alterado.
+    Devolve o próprio prado.
+    (prado x posicao x posicao --> prado)
+    """
+
+    i = obter_indice_pos_tuplo(m['pos_animais'], p1)
+    if i != -1 and eh_posicao_livre(m, p2):
+        m['pos_animais'] = m['pos_animais'][:i] + (p2,) + m['pos_animais'][i+1:]
 
     return m
 
 
 def inserir_animal(m, a, p):
-    m['animais'] += (a,)
-    m['pos_animais'] += (p,)
+    """Insere um novo animal numa posicao
+
+    Recebe um prado, uma variável do tipo animal e uma variável do tipo posicao
+    e altera destrutivamente o prado, inserindo o novo animal na posicao indicada.
+    Caso a posicao já esteja ocupada, nada é alterado.
+    Devolve o próprio prado.
+    (prado x animal x posicao --> prado)
+    """
+
+    if eh_posicao_livre(m, p):
+        m['animais'] += (a,)
+        m['pos_animais'] += (p,)
 
     return m
 
 
 def eh_prado(arg):
-            # Propriedades do dicionário
-            # Chaves do dicionário
-            # Tipo dos valores
+    """Determina se uma variavel corresponde ao tipo prado
 
-            # Tamanho dos valores que são tuplos
-            # Tipo dos elementos dos valores que são tuplos
+    Recebe um argumento de qualquer tipo e devolve True se o mesmo corresponde
+    a um TAD prado. Caso contrário devolve False.
+    Condições verificadas:
+        1. Tipo do argumento
+        2. Chaves do dicionário
+        3. Tamanho dos tuplos
+        4. Tipos dos elementos dos tuplos
+        5. Se existem rochedos nos limites exteriores
+        6. Se existem animais nos limites exteriores
+        7. Se existem animais em rochedos
+        8. Se existem posições de animais/rochedos repetidas
+    (universal --> booleano)
+    """
 
     return isinstance(arg, dict) and len(arg) == 4 and \
             'ult_pos' in arg and 'rochedos' in arg and 'animais' in arg and 'pos_animais' in arg \
             and eh_posicao(arg['ult_pos']) and isinstance(arg['rochedos'], tuple) and \
             isinstance(arg['animais'], tuple) and isinstance(arg['pos_animais'], tuple) and \
             len(arg['animais']) > 0 and len(arg['animais']) == len(arg['pos_animais']) and \
-            all(map(lambda x: eh_posicao(x), arg['rochedos'])) and \
-            all(map(lambda x,y: eh_animal(x) and eh_posicao(y), arg['animais'], arg['pos_animais']))
+            all(map(lambda x: eh_posicao(x) and not eh_limite_exterior_prado(arg['ult_pos'], x), 
+                arg['rochedos'])) and \
+            all(map(lambda x, y: eh_animal(x) and eh_posicao(y)
+                and not eh_limite_exterior_prado(arg['ult_pos'], y)
+                and obter_indice_pos_tuplo(arg['rochedos'], y) == -1, 
+                arg['animais'], arg['pos_animais'])) and \
+            not existe_pos_repetida(arg['rochedos']) and \
+            not existe_pos_repetida(arg['pos_animais'])
 
 
 def eh_posicao_animal(m, p):
+    """Verifica se existe um animal na posicao
+
+    Recebe um prado e uma posicao e devolve True se existir um animal
+    nessa posicao. Caso contrário, devolve False.
+    (prado x posicao --> booleano)
+    """
+
     return obter_indice_pos_tuplo(m['pos_animais'], p) != -1
 
 
 def eh_posicao_obstaculo(m, p):
-    # VERIFICAR SE PERTENCE AO EXTERIOR?
-    return obter_indice_pos_tuplo(m['rochedos'], p) != -1
+    """Verifica se existe um obstáculo na posicao
+
+    Recebe um prado e uma posicao e devolve True se existir um obstáculo
+    (rochedo/montanha) nessa posicao. Caso contrário, devolve False.
+    (prado x posicao --> booleano)
+    """
+
+    return obter_indice_pos_tuplo(m['rochedos'], p) != -1 or eh_limite_exterior_prado(m['ult_pos'], p)
 
 
 def eh_posicao_livre(m, p):
+    """Verifica se a posicao está livre
+
+    Recebe um prado e uma posicao e devolve True se não existir nem um obstáculo
+    (rochedo/montanha) nem um animal nessa posicao (ou seja, está livre).
+    Caso contrário, devolve False.
+    (prado x posicao --> booleano)
+    """
+
     return not (eh_posicao_animal(m, p) or eh_posicao_obstaculo(m, p))
 
 
-# CHECK
 def prados_iguais(p1, p2):
-    # O prado é constituido pelos TADs posicao e animal, pelo que não pode ser simplesmente
-    # comparado com ==
-            # Posições dos rochedos
+    """Verifica se os prados são iguais
 
-            # Animais e posições dos animais
+    Recebe dois TAD prado e devolve True apenas se ambos representam o mesmo prado.
+    Caso contrário devolve False.
+    (prado x prado --> booleano)
+    """
 
-
+    # O prado é constituido pelos TADs posicao e animal, pelo que não pode ser comparado com '=='
     return eh_prado(p1) and eh_prado(p2) and \
+            posicoes_iguais(p1['ult_pos'], p2['ult_pos']) and \
             len(p1['rochedos']) == len(p2['rochedos']) and \
             all(map(lambda x, y: posicoes_iguais(x, y), p1['rochedos'], p2['rochedos'])) and \
             len(p1['animal']) == len(p2['animal']) and \
@@ -528,6 +665,12 @@ def prados_iguais(p1, p2):
 
 
 def prado_para_str(m):
+    """Representação externa do tipo prado
+
+    Devolve uma string que corresponde à representação externa do TAD prado.
+    (prado --> str)
+    """
+
     tamanho_x = obter_tamanho_x(m)
 
     # topo
@@ -562,17 +705,37 @@ def prado_para_str(m):
 
 
 def obter_valor_numerico(m, p):
+    """Devolve o número da posicao correspondente à ordem de leitura
+
+    Recebe um prado e uma posicao e devolve o número da posicao correspondente a essa,
+    de acordo com a ordem de leitura do prado.
+    (prado x posicao --> int)
+    """
+
     return obter_pos_y(p) * obter_tamanho_x(m) + obter_pos_x(p)
 
 
 def obter_movimento(m, p):
+    """Devolve a posicao seguinte do animal
+
+    Recebe um prado e uma posicao e devolve a posicao seguinte do animal que se encontra
+    na posicao recebida, de acordo com as regras de movimento dos animais do prado.
+    Caso o animal não exista ou não existam posições livres para se mover, devolve None.
+    (prado x posicao --> posicao)
+    """
+
+    if not eh_posicao_animal(m, p):
+        return None
+
     movs_possiveis = obter_posicoes_adjacentes(p)
 
     # predador
     if eh_predador(obter_animal(m, p)):
+        # se for possível, comer presa
         for mp in movs_possiveis:
             if eh_presa(obter_animal(m, mp)):
                 return mp
+
         # retirar obstáculos
         movs_possiveis = tuple(filter(lambda x: not eh_posicao_obstaculo(m, x), movs_possiveis))
     # presa
@@ -580,5 +743,7 @@ def obter_movimento(m, p):
         # retirar obstáculos/animais
         movs_possiveis = tuple(filter(lambda x: eh_posicao_livre(m, x), movs_possiveis))   
 
+    if movs_possiveis == ():
+        return None
     return movs_possiveis[0]
 
---
+++ LEIC-A/FP/proj2/tests.py
@@ -0,0 +1,18 @@
+from main import *
+
+dim = cria_posicao(11, 4)
+obs = (cria_posicao(4,2), cria_posicao(5,2))
+an1 = tuple(cria_animal('rabbit', 5, 0) for i in range(3))
+an2 = (cria_animal('lynx', 20, 15),)
+pos = tuple(cria_posicao(p[0],p[1]) for p in ((5,1),(7,2),(10,1),(6,1)))
+prado = cria_prado(dim, obs, an1+an2, pos)
+print(obter_tamanho_x(prado), obter_tamanho_y(prado))
+print(prado_para_str(prado))
+p1 = cria_posicao(7,2)
+p2 = cria_posicao(9,3)
+prado = mover_animal(prado, p1, p2)
+print(prado_para_str(prado))
+print(obter_valor_numerico(prado, cria_posicao(9,3)))
+print(posicao_para_str(obter_movimento(prado, cria_posicao(5,1))))
+print(posicao_para_str(obter_movimento(prado, cria_posicao(6,1))))
+print(posicao_para_str(obter_movimento(prado, cria_posicao(10,1))))
