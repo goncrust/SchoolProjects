82066f2cf63b01d32c3490e8202370ab63ce5a06
goncrust <goncalonrua@protonmail.com>
2022-01-13 00:17:37 +0000

    fix

--- LEIC-A/LP/substitui.pl
+++ LEIC-A/LP/substitui.pl
@@ -1,4 +1,4 @@
 % substitui(L1, El, Novo, L2)
 substitui([], _, _, []).
 substitui([El | R1], El, Novo, [Novo | R2]) :- substitui(R1, El, Novo, R2).
-substitui([P | R1], El, Novo, [P | R2]) :- substitui(R1, El, Novo, R2).
+substitui([P | R1], El, Novo, [P | R2]) :- P \== El, substitui(R1, El, Novo, R2).
