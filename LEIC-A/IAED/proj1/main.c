a508e72531886b6727ff9fcc5e054200a32922b9
goncrust <goncalonrua@gmail.com>
2022-03-25 21:48:13 +0000

    proj1 more progress

--- LEIC-A/IAED/proj1/main.c
+++ LEIC-A/IAED/proj1/main.c
@@ -30,10 +30,10 @@
 
 #define AIRPORTS_MAX 40
 #define FLIGHTS_MAX 30000
-#define AIRPORTID_SIZE 3
+#define AIRPORTID_SIZE 4
 #define COUNTRY_SIZE 30
 #define CITY_SIZE 50
-#define FLIGHTID_SIZE 6
+#define FLIGHTID_SIZE 7
 
 /* Structures */
 
@@ -64,25 +64,147 @@ typedef struct {
     int passengers_max;
 } Flight;
 
+int add_airport(Airport airports[], int airport_count) {
+    int i, len;
+    char space;
+    Airport new_airport;
+
+    scanf("%s%s%c", new_airport.id, new_airport.country, &space);
+    fgets(new_airport.city, CITY_SIZE, stdin);
+
+    /* Replace \n with \0 */
+    len = strlen(new_airport.city);
+    new_airport.city[len-1] = '\0';
+
+    /* Errors */
+    for (i = 0; i < AIRPORTID_SIZE-1; i++) {
+        if (!isupper(new_airport.id[i])) {
+            printf("invalid airport ID\n");
+            return 1;
+        }
+    }
+
+    if (airport_count >= AIRPORTS_MAX) {
+        printf("too many airports\n");
+        return 1;
+    }
+
+    for (i = 0; i < airport_count; i++) {
+        if (strcmp(new_airport.id, airports[i].id) == 0) {
+            printf("duplicate airport\n");
+            return 1;
+        }
+    }
+
+    strncpy(airports[airport_count].id, new_airport.id, AIRPORTID_SIZE);
+    strncpy(airports[airport_count].country, new_airport.country, COUNTRY_SIZE);
+    strncpy(airports[airport_count].city, new_airport.city, CITY_SIZE);
+
+    printf("airport %s\n", airports[airport_count].id);
+
+    return 0;
+}
+
+void print_airport_by_id(char id[], Airport airports[], int airport_count) {
+    int i;
+    for (i = 0; i < airport_count; i++) {
+        if (strcmp(airports[i].id, id) == 0) {
+            printf("%s %s %s\n", airports[i].id, airports[i].city, airports[i].country); 
+            return;
+        }
+    }
+
+    printf("no such airport ID\n");
+}
+
+int alpha_before(char w1[], char w2[], int word_size) {
+    int i;
+    for (i = 0; i < word_size; i++) {
+        if (w1[i] < w2[i])
+            return 1;
+        else if (w1[i] > w2[i])
+            return 0;
+    }
+
+    return 0;
+}
+
+int list_airports(Airport airports[], int airport_count) {
+    int sort[AIRPORTS_MAX], i, j, aux;
+
+    /* Reset sort vector */
+    for (i = 0; i < airport_count; i++) sort[i] = i;
+
+    for (i = airport_count-1; i > 0; i--) {
+        for (j = 0; j < i; j++) {
+            if (alpha_before(airports[sort[j+1]].id,
+                        airports[sort[j]].id, AIRPORTID_SIZE-1)) {
+                aux = sort[j];
+                sort[j] = sort[j+1];
+                sort[j+1] = aux;
+            }
+        }
+    }
+
+    for (i = 0; i < airport_count; i++)
+        printf("%s %s %s\n", airports[sort[i]].id, airports[sort[i]].city,
+                airports[sort[i]].country); 
+
+    return 0;
+}
+
+int list_airports_specified(Airport airports[], int airport_count) {
+    char id[AIRPORTID_SIZE];
+    int i = 0, end = 0;
+
+    while (!end) {
+        id[i] = getchar();
+        if (id[i] == '\n')
+            end = 1;
+
+        if (i == 3) {
+            id[3] = '\0';
+            print_airport_by_id(id, airports, airport_count);
+            i = 0;
+            continue;
+        }
+
+        i++;
+    }
+
+    return 0;
+}
+
 int main() {
     Airport airports[AIRPORTS_MAX];
-    Flight flights[FLIGHTS_MAX];
+    int airport_count = 0;
+    /* Flight flights[FLIGHTS_MAX]; */
 
+    /* running - 0 if program termanted */
+    /* error - used to store the return of functions (0 - success, 1 - error) */
+    int running = 1, error;
+    
     /* Main Loop */
-    int running = 1;
     while (running) {
 
         /* Read command */
-        char command;
-        scanf("%c", &command);
+        char command, last_char;
+        scanf("%c%c", &command, &last_char);
 
         switch (command) {
-            case 'd':
+            case 'q':
                 running = 0;
                 break;
             case 'a':
+                error = add_airport(airports, airport_count);
+                if (!error)
+                    airport_count++;
                 break;
             case 'l':
+                if (last_char == ' ')
+                    list_airports_specified(airports, airport_count);
+                else if (last_char == '\n')
+                    list_airports(airports, airport_count);
                 break;
             case 'v':
                 break;
