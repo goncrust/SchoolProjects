091d1517ebdf3e706e6a567602fbfe826f420b4c
goncrust <goncalonrua@protonmail.com>
2022-11-24 11:26:41 +0000

    [IAED] organize algos

--- LEIC-A/IAED/algos/sort/my/myinsertion.c
+++ LEIC-A/IAED/algos/sort/my/myinsertion.c
@@ -1,11 +1,9 @@
-/* j >= 0!! j+1!! */
-
 void myinsertion_sort(int a[], int n) {
     int i, j, temp;
-    for (i = 0; i < n; i++) {
+    for (i = 1; i < n; i++) {
         temp = a[i];
         j = i-1;
-        while (temp < a[j]) {
+        while (j >= 0 && temp < a[j]) {
              a[j+1] = a[j];
              j--;
         }
